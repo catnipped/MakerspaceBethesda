ff6d4b3adaeceec911342e2fe5dd76a02dcd09b5
Casper Liljegren <casper.liljegren@elev.ga.lbs.se>
2018-08-30 11:07:25 +0200

    PlayerShoot
    +Shoot

---
+++ MakerspaceBethesda/Assets/PlayerShoot.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShoot : MonoBehaviour
+{
+    public float fireRate = 0.1f;
+    public float fireCooldown = 0f;
+    public float gunSpread = 5f;
+
+    public GameObject projectilePrefab;
+
+    public Transform point;
+
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (Input.GetButton("Fire"))
+        {
+            if (CanFire())
+            {
+                Fire();
+            }
+        }
+    }
+
+    //kollar om min cooldown är klar
+    private bool CanFire()
+    {
+        return fireCooldown <= 0;
+    }
+
+    //"spawnar" en kula samt skjuter
+    private void Fire()
+    {
+        Instantiate(projectilePrefab, point.position, AngleWithSpread(gunSpread));
+        fireCooldown = fireRate;
+    }
+
+    //avancerad matte
+    private Quaternion AngleWithSpread(float spread)
+    {
+        return (point.rotation * Quaternion.Euler(0, 0, Random.Range(-spread, spread)));
+    }
+}
