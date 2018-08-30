8c5e78ed1a2232b9920797016d1b7bccb54d8c76
Filip Klevell <f.klevell@gmail.com>
2018-08-30 11:14:35 +0200

    La till Shortcuts för A o D

--- MakerspaceBethesda/Assets/Movementscript.cs
+++ MakerspaceBethesda/Assets/Movementscript.cs
@@ -2,15 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour {
+public class NewBehaviourScript : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        KeyCode RightKey = KeyCode.D;
+        KeyCode LeftKey = KeyCode.A;
+    }
 }
