034b99f46592a4e7f3364e0b1dd811bfdf384c7a
Filip Klevell <f.klevell@gmail.com>
2018-09-06 09:33:08 +0200

    Revert "La till Shortcuts för A o D"
    This reverts commit 8c5e78ed1a2232b9920797016d1b7bccb54d8c76.

--- MakerspaceBethesda/Assets/Movementscript.cs
+++ MakerspaceBethesda/Assets/Movementscript.cs
@@ -2,19 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
-{
+public class NewBehaviourScript : MonoBehaviour {
 
-    // Use this for initialization
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        KeyCode RightKey = KeyCode.D;
-        KeyCode LeftKey = KeyCode.A;
-    }
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
 }
